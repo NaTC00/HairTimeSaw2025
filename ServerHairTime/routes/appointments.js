9629474910729968c2cd624df9a0c211f21d12bc
natalia.ceccarini <natalia.ceccarini@intouch-srl.com>
2025-05-26 16:10:58 +0200

    Implemented API for retrieving and deleting user's appointments

--- ServerHairTime/routes/appointments.js
+++ ServerHairTime/routes/appointments.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const pool = require('../db');
 const admin = require('firebase-admin');
 const { verifyToken } = require('./auth');
-const { error } = require('console');
+const { error, group } = require('console');
+const { getAppoinmentsUser, getTotalDuration, getAppointmentsByDate, deleteAppointmentUser } = require('../sql/queries');
+const { start } = require('repl');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 
 // POST /appointments/create-service → crea un servizio
@@ -44,6 +51,7 @@ router.get('/services', async (req, res) => {
   }
 });
 
+// TODO: controllare prima la disponibilità della data e la fascia oraria
 router.post('/book', verifyToken, async (req, res) => {
   console.log("Richiesta prenotazione appuntamento");
 
@@ -74,14 +82,117 @@ router.post('/book', verifyToken, async (req, res) => {
     }
 
     await client.query('COMMIT');
-    res.status(201).json({ message: 'Appuntamento creato', appointmentId });
+    return res.status(201).json({ message: 'Appuntamento creato', appointmentId });
   } catch (err) {
     await client.query('ROLLBACK');
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   } finally {
     client.release();
   }
 });
+
+router.delete('/:id', verifyToken, async (req, res) => {
+
+  const appointmentId = req.params.id;
+  const userId = req.user.uid;
+  console.log(`Elimino appuntamento ${appointmentId} per utente ${userId}`);
+  try {
+
+    await deleteAppointmentUser(userId, appointmentId);
+    return res.status(200).json({ message: 'Appuntamento eliminato' });
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+
+
+router.get('/user', verifyToken, async (req, res) => {
+  const userId = req.user.uid;
+
+  try {
+
+    const rawAppointments = await getAppoinmentsUser(userId);
+
+     const formattedAppointments = rawAppointments.map(appt => ({
+      ...appt,
+      date: dayjs.utc(appt.date).tz("Europe/Rome").format("YYYY-MM-DD")
+    }));
+
+
+
+  
+    return res.status(200).json(formattedAppointments);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+
+function toTime(minutes) {
+  const h = String(Math.floor(minutes / 60)).padStart(2, '0');
+  const m = String(minutes % 60).padStart(2, '0');
+  return `${h}:${m}`;
+}
+
+router.post('/availability', async (req, res) => {
+  const {services} = req.body
+  if(!Array.isArray(services) || services.length === 0){
+    return res.status(400).json({error: 'Servizi richiesti non validi'});
+  }
+  try{
+    const totalDuration = parseInt(await getTotalDuration(services), 10);
+    if(!totalDuration) return res.status(500).json({ error: err.message }); 
+
+    const availability = {};
+    const today = new Date()
+
+    for(let dayOffset = 0; dayOffset < 60; dayOffset++){
+      const date = new Date(today);
+      date.setDate(today.getDate() + dayOffset)
+
+      const dateStr = date.toISOString().split('T')[0]
+      const dayAvailability = []
+
+      const timeBlocks = [
+        {start: 9 * 60, end: 13 * 60},
+        {start: 14 * 60, end: 18 *60}
+      ]
+
+      const existingAppointments = await getAppointmentsByDate(dateStr)
+
+      const occupiedSlots = existingAppointments.map(row => {
+        const [from, to] = row.time_slot.split('-').map(hm => {
+          const [h, m] = hm.split(':').map(Number)
+          return h * 60 + m
+        })
+        return {from, to}
+      })
+
+      for (const block of timeBlocks){
+        for(let t = block.start; t + totalDuration <= block.end; t+=30){
+          const from = t;
+          const to = t + totalDuration;
+          const overlap = occupiedSlots.some(slot => !(to <= slot.from || from >= slot.to))
+          if(!overlap) dayAvailability.push(`${toTime(from)}-${toTime(to)}`)
+        }
+      }
+
+      if(dayAvailability.length > 0){
+        availability[dateStr] = dayAvailability
+      }
+
+
+    }
+
+    return res.json(availability)
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
 
 
 module.exports = router;
---
+++ ServerHairTime/sql/queries.js
@@ -0,0 +1,54 @@
+const pool = require('../db')
+
+async function getAppoinmentsUser(userId){
+    const result = await pool.query(
+       `SELECT 
+        a.id, 
+        a.date AS date,
+        a.time_slot, 
+        a.created_at,
+        json_agg(s.name) AS services
+      FROM appointments a
+      JOIN appointment_services aps ON a.id = aps.appointment_id
+      JOIN services s ON aps.service_id = s.id
+      WHERE a.user_id = $1
+      GROUP BY a.id
+      ORDER BY a.date, a.time_slot`, [userId]);
+      
+      return result.rows
+}
+
+async function deleteAppointmentUser(userId, appointmentId) {
+    const result = await pool.query(
+        `DELETE
+        FROM appointments
+        WHERE  user_id = $1 AND id = $2
+        `, [userId, appointmentId]) //posso fare questo perchè nella tabella appointment_services ho usato ON DELETE CASCADE
+    
+}
+
+
+async function getTotalDuration(serviceIds) {
+    const result = await pool.query(
+        `SELECT SUM(duration_minutes) AS total_duration
+        FROM services
+        WHERE id = ANY($1)
+        `, [serviceIds])
+    return result.rows[0].total_duration
+}
+
+async function getAppoinmentByDate(date) {
+    const result = await pool.query(
+        `SELECT time_slot 
+        FROM appointments
+        WHERE date = $1 
+        `, [date])
+    return result.rows
+}
+
+module.exports = {
+    getAppoinmentsUser,
+    getTotalDuration,
+    getAppoinmentByDate,
+    deleteAppointmentUser
+}
